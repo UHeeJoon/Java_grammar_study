9da585ed55dff6c56785f9c13b5fe7a00e5d9a76
UHeeJoon <yhj3855@naver.com>
2022-01-17 00:03:19 +0900

    ADD README.md

---
+++ src/Java100_variable_HelloWorld.java
@@ -0,0 +1,21 @@
+public class Java100_variable_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hellow World");
+    }
+}
+/*
+// 배열의 복사
+import java.util.Array;
+배열 복사는 System.arraycopy(원본 배열, 범위, 복사배열 , 범위, 길이);
+int[] arr = {1,2,3,4,5};
+int[] arr2 = {1,2,3,4,5,6,7,8,9,10};
+System.arraycopy(arr, 2, arr2 , 4 ,3);
+==> arr2 = [1,2,3,4,3,4,5,8,9,10};
+System.out.println(Arrays.toString(arr2));
+
+
+// 문자열 쪼개기 charAt();
+String arr = "name";
+System.out.println(arr.charAt(1));
+==> a
+ */
